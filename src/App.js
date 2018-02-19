32c1398fbd2f6f3e6653a28ccc4f6ef837f3651f
Jared Johnson <jared@nuts.com>
2018-02-18 19:42:56 -0500

    Create project.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Story Teller</h1>
+          <p>Hello, world!</p>
+        </header>
+        <main>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
