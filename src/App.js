f1979984a890747178c371870d796b672e631ae1
Jared Johnson <jared@nuts.com>
2018-02-18 20:00:01 -0500

    Simplify App.js and App.css.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Story Teller</h1>
-          <p>Hello, world!</p>
-        </header>
-        <main>
-        </main>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <header>
+      <h1>Story Teller</h1>
+      <p>Hello, world!</p>
+    </header>
+    <main>
+    </main>
+  </div>
+);
 
 export default App;
