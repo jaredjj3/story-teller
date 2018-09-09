3f38595642d52f38543330b875acdbbe85379bcf
jaredjj3 <jared@nuts.com>
2018-09-09 15:58:17 -0400

    Create THEME constant.

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -1,12 +1,15 @@
 import { Layout } from 'antd';
 import * as React from 'react';
 import styled from 'react-emotion';
+import { THEME } from 'constants/index';
 
 const Style = styled('div')`
   color: white;
   font-weight: lighter;
   font-size: 24px;
   letter-spacing: 2px;
+  margin: 0 auto;
+  max-width: ${() => THEME.maxWidth};
 `;
 
 export const Header = () => (
