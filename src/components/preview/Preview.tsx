dbaeab7369ce1f58e3041c336ff810c0d59c278d
jaredjj3 <jared@nuts.com>
2018-09-09 17:10:49 -0400

    Implement react context.

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import { ContentContext } from '../app/Content';
+import styled from 'react-emotion';
+
+const Style = styled('div')`
+  padding-left: 12px;
+`;
 
 export const Preview = () => (
-  <div>
-    <h2>preview</h2>
-  </div>
+  <Style>
+    <ContentContext.Consumer>
+      {
+        ctx => (
+          <div>
+            <h1>{ctx.imgSrc}</h1>
+          </div>
+        )
+      }
+    </ContentContext.Consumer>
+  </Style>
 );
