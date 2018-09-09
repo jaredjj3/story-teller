5630259268e4418208a380ad6e1f1510d3b91b75
jaredjj3 <jared@nuts.com>
2018-09-09 17:10:49 -0400

    Implement react context.

--- src/components/app/Content.tsx
+++ src/components/app/Content.tsx
@@ -3,25 +3,52 @@ import * as React from 'react';
 import styled from 'react-emotion';
 import { Editor, Preview } from 'components';
 import { THEME } from 'constants/index';
+import { compose, withState, withHandlers } from 'recompose';
+
+interface IWithStateProps {
+  imgSrc: string;
+  setImgSrc: (imgSrc: string) => void;
+}
+
+interface IWithHandlerProps extends IWithStateProps {
+  handleImgSrcChange: (event: React.ChangeEvent) => void;
+}
+
+const enhance = compose<IWithHandlerProps, {}>(
+  withState('imgSrc', 'setImgSrc', ''),
+  withHandlers({
+    handleImgSrcChange: (props: IWithStateProps) => (event: any) => {
+      props.setImgSrc(event.target.value);
+    }
+  })
+);
+
+export const ContentContext = React.createContext({
+  imgSrc: '',
+  handleImgSrcChange: (event: React.ChangeEvent) => { return; },
+  setImgSrc: (imgSrc: string) => { return; }
+});
 
 const Style = styled('div')`
   min-height: 100vh;
-  margin: 0 auto;
   max-width: ${() => THEME.maxWidth};
-  margin-top: 24px;
+  margin: 0 auto;
+  padding-top: 50px;
 `;
 
-export const Content = () => (
-  <Layout.Content>
+export const Content = enhance(props => (
+  <Layout.Content style={{ padding: '0 50px' }}>
     <Style>
-      <Row>
-        <Col span={16}>
-          <Editor />
-        </Col>
-        <Col span={8}>
-          <Preview />
-        </Col>
-      </Row>
+      <ContentContext.Provider value={props}>
+        <Row>
+          <Col span={6}>
+            <Editor />
+          </Col>
+          <Col span={18}>
+            <Preview />
+          </Col>
+        </Row>
+      </ContentContext.Provider>
     </Style>
   </Layout.Content>
-);
+));
--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,7 +1,27 @@
 import * as React from 'react';
+import { ContentContext } from '../app/Content';
+import { Form, Input } from 'antd';
+import styled from 'react-emotion';
+
+const Style = styled('div')`
+  padding-right: 12px;
+`;
 
 export const Editor = () => (
-  <div>
-    <h2>editor</h2>
-  </div>
+  <Style>
+    <ContentContext.Consumer>
+      {
+        ctx => (
+          <Form>
+            <Form.Item label="src">
+              <Input
+                value={ctx.imgSrc}
+                onChange={ctx.handleImgSrcChange}
+              />
+            </Form.Item>
+          </Form>
+        )
+      }
+    </ContentContext.Consumer>
+  </Style>
 );
