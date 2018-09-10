e4e82c72c8f620ff8fa3fafd168386fd6b1ef822
Jared Johnson <jared@nuts.com>
2018-09-10 09:22:47 -0400

    Do not render the preview if the image src is not valid.

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,12 +1,47 @@
 import * as React from 'react';
 import styled from 'react-emotion';
+import { compose, branch, renderNothing, withState, lifecycle, withHandlers } from 'recompose';
+
+interface IOuterProps {
+  src: string;
+}
+
+interface IWithStateProps extends IOuterProps {
+  isSrcValid: boolean;
+  setIsSrcValid: (isSrcValid: boolean) => void;
+}
+
+interface IWithHandlerProps extends IWithStateProps {
+  testImgSrc: () => void;
+}
+
+const enhance = compose<IWithHandlerProps, IOuterProps>(
+  withState('isSrcValid', 'setIsSrcValid', false),
+  withHandlers({
+    testImgSrc: (props: IWithStateProps) => () => {
+      const img = document.createElement('img');
+      img.onerror = () => props.setIsSrcValid(false);
+      img.onload = () => props.setIsSrcValid(true);
+      img.src = props.src;
+    }
+  }),
+  lifecycle<IWithHandlerProps, {}>({
+    componentDidMount() {
+      this.props.testImgSrc();
+    },
+    componentDidUpdate() {
+      this.props.testImgSrc();
+    }
+  }),
+  branch<IWithStateProps>(props => !props.isSrcValid, renderNothing)
+);
 
 const Style = styled('div')`
   padding-left: 12px;
 `;
 
-export const Preview = () => (
+export const Preview = enhance(props => (
   <Style>
-    Preview
+    <img src={props.src} />
   </Style>
-);
+));
