0cb49b0b4a06feeade53c73a620cbf5d8ba0fe00
Jared Johnson <jared@nuts.com>
2018-09-10 08:48:38 -0400

    Slim down the Preview component.

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,38 +1,12 @@
 import * as React from 'react';
-import { ContentContext } from '../app/Content';
 import styled from 'react-emotion';
-
-const SIZE = '640px';
 
 const Style = styled('div')`
   padding-left: 12px;
 `;
 
-const ImgContainer = styled('div')`
-  width: ${() => SIZE};
-  height: ${() => SIZE};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 4px solid lime;
-  background-image: linear-gradient(red, yellow);
-`;
-
-const OuterImg = styled('img')`
-  height: 240px;
-  width: 240px;
-`;
-
 export const Preview = () => (
   <Style>
-    <ContentContext.Consumer>
-      {
-        ctx => (
-          <ImgContainer>
-            <OuterImg src={ctx.imgSrc} />
-          </ImgContainer>
-        )
-      }
-    </ContentContext.Consumer>
+    Preview
   </Style>
 );
