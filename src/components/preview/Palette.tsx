6e134eaa91c6704e8001f5159884add302a779a3
Jared Johnson <jared@nuts.com>
2018-09-10 08:52:48 -0400

    Get rid of ContentContext.

--- src/components/preview/Palette.tsx
+++ src/components/preview/Palette.tsx
@@ -1,16 +1,7 @@
 import * as React from 'react';
-import { ContentContext } from '../app/Content';
 
 export const Palette = () => (
-  <ContentContext.Consumer>
-    {
-      ctx => (
-        <div>
-          <h1>{ctx.color}</h1>
-          <h1>{ctx.backgroundColor}</h1>
-          <h1>{ctx.alternativeColor}</h1>
-        </div>
-      )
-    }
-  </ContentContext.Consumer>
+  <div>
+    Palette
+  </div>
 );
