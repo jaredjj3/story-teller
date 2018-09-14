2bf5f4ee3769f803d5d5fb0a8c32453509f651d3
Jared Johnson <jared@nuts.com>
2018-09-13 20:09:44 -0400

    Sync palette.

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -3,21 +3,38 @@ import { Row, Col, Form, Input } from 'antd';
 import styled from 'react-emotion';
 import { Preview } from '../preview';
 import { compose, withState, withHandlers } from 'recompose';
+import { IPalette } from '../../types/palette';
 
 interface IWithStateProps {
   src: string;
+  color: string;
+  backgroundColor: string;
+  alternativeColor: string;
   setSrc: (src: string) => void;
+  setColor: (color: string) => void;
+  setBackgroundColor: (backgroundColor: string) => void;
+  setAlternativeColor: (alternativeColor: string) => void;
 }
 
 interface IWithHandlerProps extends IWithStateProps {
   handleSrcChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handlePaletteChange: (palette: IPalette) => void;
 }
 
 const enhance = compose<IWithHandlerProps, {}>(
   withState('src', 'setSrc', 'https://i.scdn.co/image/c86f5f7a542e81c14ec6a65f009a2ab801e32272'),
+  withState('color', 'setColor', 'black'),
+  withState('backgroundColor', 'setBackgroundColor', 'white'),
+  withState('alternativeColor', 'setAlternativeColor', 'fuschia'),
   withHandlers({
     handleSrcChange: (props: IWithStateProps) => (event: React.ChangeEvent<HTMLInputElement>) => {
       props.setSrc(event.currentTarget.value);
+    },
+    handlePaletteChange: (props: IWithStateProps) => (palette: IPalette) => {
+      const { color, backgroundColor, alternativeColor } = palette;
+      props.setColor(color);
+      props.setBackgroundColor(backgroundColor);
+      props.setAlternativeColor(alternativeColor);
     }
   })
 );
@@ -28,7 +45,7 @@ const Style = styled('div')`
 
 export const Editor = enhance(props => (
   <Style>
-    <Row>
+    <Row gutter={24}>
       <Col span={6}>
         <Form>
           <Form.Item label="src">
@@ -37,7 +54,10 @@ export const Editor = enhance(props => (
         </Form>
       </Col>
       <Col span={18}>
-        <Preview src={props.src} />
+        <Preview
+          src={props.src}
+          onPaletteChange={props.handlePaletteChange}
+        />
       </Col>
     </Row>
   </Style>
