f5851b8d65fe940ce7b80a985dd177a5943a76de
jaredjj3 <jared@nuts.com>
2018-09-15 19:36:09 -0400

    Connect songName and artistName.

---
+++ src/constants/DEFAULT_TEXT_SPECS.ts
@@ -0,0 +1,14 @@
+import { ITextSpec } from '../types/text-spec';
+
+export const DEFAULT_TEXT_SPECS: ITextSpec[] = [
+  {
+    text: 'good things take time',
+    from: 1000,
+    to: 4000
+  },
+  {
+    text: 'don\'t rush',
+    from: 4000,
+    to: 6000
+  }
+];
