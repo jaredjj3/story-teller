b589596a0696ccb17a01a9e19a763174cf66ffd2
Jared Johnson <jared@nuts.com>
2018-09-15 10:54:59 -0400

    Fix palette setting.

--- src/components/preview/SyncPalette.tsx
+++ src/components/preview/SyncPalette.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { compose, lifecycle } from 'recompose';
+import { compose, lifecycle, shouldUpdate } from 'recompose';
 import { IPalette } from 'types/palette';
+import { isEqual } from 'lodash';
 
 interface IOuterProps {
   palette: IPalette;
@@ -8,6 +9,7 @@ interface IOuterProps {
 }
 
 const enhance = compose<IOuterProps, IOuterProps>(
+  shouldUpdate<IOuterProps>((props, nextProps) => !isEqual(props.palette, nextProps.palette)),
   lifecycle<IOuterProps, {}>({
     componentDidMount() {
       this.props.onPaletteChange(this.props.palette);
