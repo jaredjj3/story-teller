8b85f7cce7f10822fbdb98d37cb99b318ce816b7
jaredjj3 <jared@nuts.com>
2018-09-15 21:39:36 -0400

    Finish first draft.

--- src/constants/DEFAULT_TEXT_SPECS.ts
+++ src/constants/DEFAULT_TEXT_SPECS.ts
@@ -3,27 +3,27 @@ import { ITextSpec } from '../types/text-spec';
 export const DEFAULT_TEXT_SPECS: ITextSpec[] = [
   {
     text: 'good things take time',
-    from: 750,
+    from: 650,
     to: 4000
-  },
-  {
+  }, {
     text: '',
     from: 4000,
-    to: 6000
-  },
-  {
+    to: 5850
+  }, {
     text: 'don\'t',
-    from: 6000,
+    from: 5850,
     to: 6750
-  },
-  {
+  }, {
     text: 'don\'t rush',
     from: 6750,
+    to: 7650
+  }, {
+    text: 'don\'t rush anything',
+    from: 7650,
     to: 9250
-  },
-  {
+  }, {
     text: '',
-    from: 9250,
-    to: 11000
+    from: 9850,
+    to: 10850
   }
 ];
